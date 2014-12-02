6dd1b0b7be5b18fa56e5d7866d7e28918f98c0e6
LiBo520 <751934332@qq.com>
2014-12-02 16:15:33 +0800

    选择医院

---
+++ PhysicianAssistant/Hospital/LYHospitalList.h
@@ -0,0 +1,13 @@
+//
+//  LYHospitalList.h
+//  PhysicianAssistant
+//
+//  Created by langyi on 14/12/2.
+//  Copyright (c) 2014年 wangliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LYHospitalList : UITableViewController
+
+@end
